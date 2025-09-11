957493c4fcec5a03b648b922bfbd18b128175bbd
GabyDs <gabydsch2001@gmail.com>
2025-09-11 09:21:43 -0300

    Separate MESH configuration from main

---
+++ main/mesh_setup.h
@@ -0,0 +1,16 @@
+#ifndef MESH_SETUP_H
+#define MESH_SETUP_H
+
+#include "esp_err.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void setup_mesh(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // MESH_SETUP_H
